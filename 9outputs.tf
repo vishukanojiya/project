4618bd38cfc7a0a3e7468fcd008192a9a0bdf854
vishukanojiya <vishalkanojiya2694@gmail.com>
2026-09-23 06:05:04 +0530

    Add outputs for ALB, ECS, and RDS resources

---
+++ 9outputs.tf
@@ -0,0 +1,19 @@
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.address
+}
+
+output "rds_port" {
+  value = aws_db_instance.mysql.port
+}
