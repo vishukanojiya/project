cb8ad1b3e0b5c18a570a3861801b2c0c7e7693c7
vishukanojiya <vishalkanojiya2694@gmail.com>
2026-09-23 05:40:50 +0530

    Add security groups for ALB, ECS, and RDS
    Defined security groups for ALB, ECS, and RDS with appropriate ingress and egress rules.

---
+++ 5security-groups.tf
@@ -0,0 +1,88 @@
+# ==========================================
+# ALB SECURITY GROUP
+# ==========================================
+
+resource "aws_security_group" "alb" {
+  name   = "${var.project_name}-alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP from Internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
+  }
+}
+
+
+# ==========================================
+# ECS SECURITY GROUP
+# ==========================================
+
+resource "aws_security_group" "ecs" {
+  name   = "${var.project_name}-ecs-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow traffic only from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ecs-sg"
+  }
+}
+
+
+# ==========================================
+# RDS SECURITY GROUP
+# ==========================================
+
+resource "aws_security_group" "rds" {
+  name   = "${var.project_name}-rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow MySQL only from ECS"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-rds-sg"
+  }
+}
